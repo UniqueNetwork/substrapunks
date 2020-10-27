49c7d9fde9f9b8f2c1bdef1281191fa3781d6279
kpozdnikin <kpozdnikin@usetech.ru>
2020-10-27 14:25:46 +0300

    my page add try-catch

--- public/js/my.js
+++ public/js/my.js
@@ -6,38 +6,60 @@ function setPunkImage() {
   let img = document.getElementById("punkImg");
   img.src = `images/punks/image${punkId}.png`
 }
-function showExtensionWarning() {
-  document.getElementById("punkDetails").style.display = "none";
-  document.getElementById("extensionWarning").style.display = "block";
+
+function showError() {
+  var tokenListBlock = document.getElementById("tokenlist");
+  if (tokenListBlock) {
+    tokenListBlock.innerHTML = "Ups, something went wrong! Please, refresh this page!";
+  }
 }
-async function show() {
-  let address = document.getElementById("address").value;
-  setCookie("userSelectedAddress", address, 365);
-  document.getElementById("tokenlist").innerHTML = "Loading data from Blockchain...";
 
-  // Request address punks from blockchain
-  let n = new nft();
-  let nfts = await n.getAddressTokens(address);
-  let marketNfts = await n.getAddressTokensOnMarket(address);
-
-  let listhtml = "";
-  for (let i=marketNfts.length-1; i>=0; i--) {
-    const id = marketNfts[i].id;
-    const punk = await n.loadPunkFromChain(id);
-    punk["price"] = marketNfts[i].price;
-    listhtml += getPunkCard(id, punk, true);
-    document.getElementById("tokenlist").innerHTML = listhtml;
+function showExtensionWarning() {
+  try {
+    document.getElementById("punkDetails").style.display = "none";
+    document.getElementById("extensionWarning").style.display = "block";
+  } catch (e) {
+    console.log('showExtensionWarning error', e);
+    showError();
   }
-  for (let i=nfts.length-1; i>=0; i--) {
-    const id = nfts[i];
-    const punk = await n.loadPunkFromChain(id);
-    listhtml += getPunkCard(id, punk, false);
-    document.getElementById("tokenlist").innerHTML = listhtml;
-  }
-  if (marketNfts.length + nfts.length == 0)
-    listhtml = "No tokens in the wallet. :( Check out the <a href='index.html'>Marketplace</a>";
+}
 
-  document.getElementById("tokenlist").innerHTML = listhtml;
+async function showPunksFromAddresses() {
+  try {
+    let address = document.getElementById("address").value;
+    setCookie("userSelectedAddress", address, 365);
+    document.getElementById("tokenlist").innerHTML = "Loading data from Blockchain...";
+
+    // Request address punks from blockchain
+    let n = new nft();
+    let nfts = await n.getAddressTokens(address);
+    let marketNfts = await n.getAddressTokensOnMarket(address);
+    let listhtml = "";
+
+    for (let i = marketNfts.length - 1; i >= 0; i--) {
+      const id = marketNfts[i].id;
+      const punk = await n.loadPunkFromChain(id);
+      punk["price"] = marketNfts[i].price;
+      listhtml += getPunkCard(id, punk, true);
+      document.getElementById("tokenlist").innerHTML = listhtml;
+    }
+
+    for (let i = nfts.length-1; i >=0 ; i--) {
+      const id = nfts[i];
+      const punk = await n.loadPunkFromChain(id);
+      listhtml += getPunkCard(id, punk, false);
+      document.getElementById("tokenlist").innerHTML = listhtml;
+    }
+
+    if (marketNfts.length + nfts.length === 0) {
+      listhtml = "No tokens in the wallet. :( Check out the <a href='index.html'>Marketplace</a>";
+    }
+
+    document.getElementById("tokenlist").innerHTML = listhtml;
+  } catch (e) {
+    console.log('showPunksFromAddresses error', e);
+    showError();
+  }
 }
 
 window.onload = async function() {
@@ -48,38 +70,45 @@ window.onload = async function() {
   // setPunkImage();
   // setTitle();
 
-  let n = new nft();
-  let extensionPresent = await n.checkExtension();
-  if (!extensionPresent) {
-    showExtensionWarning();
-  } else {
-    // Populate addresses from extension
-    addrList = await n.getWalletAddresses();
-    console.log(addrList);
+ try {
+   let n = new nft();
+   let extensionPresent = await n.checkExtension();
 
-    let sel = document.getElementById('address');
-    for(let i=0; i<addrList.length; i++)
-    {
-      let addr = addrList[i];
-      console.log(addr);
-      var opt = document.createElement("option");
-      opt.value= addr.address;
-      opt.innerHTML = `${addr.meta.name} - ${addr.address}`;
-      sel.appendChild(opt);
-    }    
+   if (!extensionPresent) {
+     showExtensionWarning();
+   } else {
+     // Populate addresses from extension
+     addrList = await n.getWalletAddresses();
 
-    let userSelectedAddress = getCookie('userSelectedAddress');
-    if (userSelectedAddress) {
-      sel.value = userSelectedAddress;
-    }
+     console.log(addrList);
 
-    if (addrList.length >= 1) {
-      show();
-    }
+     let sel = document.getElementById('address');
+     for(let i=0; i < addrList.length; i++)
+     {
+       let addr = addrList[i];
+       console.log(addr);
+       var opt = document.createElement("option");
+       opt.value= addr.address;
+       opt.innerHTML = `${addr.meta.name} - ${addr.address}`;
+       sel.appendChild(opt);
+     }
 
-    // Show punk info
-    // await getPunkInfo(punkId);
-  }
+     let userSelectedAddress = getCookie('userSelectedAddress');
+     if (userSelectedAddress) {
+       sel.value = userSelectedAddress;
+     }
+
+     if (addrList.length >= 1) {
+       showPunksFromAddresses();
+     }
+
+     // Show punk info
+     // await getPunkInfo(punkId);
+   }
+ } catch (e) {
+   console.log('my page initialization error', e);
+   showError();
+ }
 
 
-}
+};
