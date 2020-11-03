5c455d65dd41bc162712ad668261238f4f189694
Greg Zaitsev <greg.zaitsev@gmail.com>
2020-11-03 20:02:40 +0300

    Add blacklist

--- market_vault/unique_vault.js
+++ market_vault/unique_vault.js
@@ -13,6 +13,9 @@ const contractAbi = require("./market_metadata.json");
 const quoteId = 2; // KSM
 const logFile = "./operations_log";
 
+const blackList = [ 7395, 1745, 8587, 573, 4732, 3248, 6986, 7202, 6079, 1732, 6494, 7553, 6840, 4541, 2102, 3503, 6560, 4269, 2659, 3912, 3470, 6290, 5811, 5209, 8322, 1813, 7771, 2578, 2661, 2983, 2119, 3310, 1547, 1740, 3187, 8194, 4651, 6188, 2167, 3487, 3106, 6070, 3446, 2407, 5870, 3745, 6389, 3246, 9385, 9680, 6457, 8462, 2350, 3927, 2269, 8485, 6198, 6787, 2047, 2197, 2379, 2466, 2558, 2682, 2759, 2979, 4232, 4273, 8187, 8190, 2935, 2673, 5228, 7683, 2075, 9845, 1645, 3198, 7490, 3192, 7907, 3167, 858, 239, 7613, 2790, 7043, 5536, 8277, 1134, 6378, 2416, 2373, 2240, 3952, 5017, 4999, 5986, 3159, 6155, 9329, 6445, 2117, 3935, 6091, 7841, 8725, 5194, 5744, 8120, 5930, 578, 6171, 6930, 2180, 6212, 5963, 7097, 8774, 5233, 7978, 2938, 2364, 1823, 1840, 8672, 5616, 737, 6122, 8769, 615, 9729, 3489, 427, 9883, 8678, 6579, 1776, 7061, 873, 5324, 2390, 6187, 9517, 2321, 3390, 3180, 6692, 2129, 9854, 1572, 7412, 3966, 1302, 1145, 1067, 3519, 7387, 8314, 648, 219, 2055, 825, 1195
+];
+
 function getTime() {
   var a = new Date();
   var hour = a.getHours();
@@ -143,6 +146,12 @@ async function registerNftDepositAsync(api, sender, depositorAddress, collection
 
   const value = 0;
   const maxgas = 1000000000000;
+
+  if (blackList.includes(token_id)) {
+    console.log(`Blacklisted NFT received. Silently returning.`);
+    log(`Blacklisted NFT received. Silently returning.`, "WARNING");
+    return;
+  }
 
   const tx = api.tx.contracts
     .call(config.marketContractAddress, value, maxgas, abi.messages.registerNftDeposit(collection_id, token_id, depositorAddress))
--- public/js/details.js
+++ public/js/details.js
@@ -3,6 +3,9 @@ let addrList = []; // List of extension addresses
 let punk;
 let marketContract;
 
+const blackList = [ 7395, 1745, 8587, 573, 4732, 3248, 6986, 7202, 6079, 1732, 6494, 7553, 6840, 4541, 2102, 3503, 6560, 4269, 2659, 3912, 3470, 6290, 5811, 5209, 8322, 1813, 7771, 2578, 2661, 2983, 2119, 3310, 1547, 1740, 3187, 8194, 4651, 6188, 2167, 3487, 3106, 6070, 3446, 2407, 5870, 3745, 6389, 3246, 9385, 9680, 6457, 8462, 2350, 3927, 2269, 8485, 6198, 6787, 2047, 2197, 2379, 2466, 2558, 2682, 2759, 2979, 4232, 4273, 8187, 8190, 2935, 2673, 5228, 7683, 2075, 9845, 1645, 3198, 7490, 3192, 7907, 3167, 858, 239, 7613, 2790, 7043, 5536, 8277, 1134, 6378, 2416, 2373, 2240, 3952, 5017, 4999, 5986, 3159, 6155, 9329, 6445, 2117, 3935, 6091, 7841, 8725, 5194, 5744, 8120, 5930, 578, 6171, 6930, 2180, 6212, 5963, 7097, 8774, 5233, 7978, 2938, 2364, 1823, 1840, 8672, 5616, 737, 6122, 8769, 615, 9729, 3489, 427, 9883, 8678, 6579, 1776, 7061, 873, 5324, 2390, 6187, 9517, 2321, 3390, 3180, 6692, 2129, 9854, 1572, 7412, 3966, 1302, 1145, 1067, 3519, 7387, 8314, 648, 219, 2055, 825, 1195
+];
+
 function resetView() {
   try {
     document.getElementById("punkDetails").style.display = "block";
@@ -315,6 +318,8 @@ function updateProgress(msg) {
 
 async function sellStep2() {
   try {
+    if (blackList.includes(punkId)) throw "This NFT was blacklisted from this marketplace due to fraudlent activities.";
+
     document.getElementById('progress').style.display = "block";
     document.getElementById('progress').innerHTML = "Mining transaction...";
 
@@ -421,6 +426,8 @@ async function canceltx() {
 
 async function buyStep2() {
   try {
+    if (blackList.includes(punkId)) throw "This NFT was blacklisted from this marketplace due to fraudlent activities.";
+
     document.getElementById('progress').style.display = "block";
     let newOwner = document.getElementById("newowner").value;
     let n = new nft();
@@ -622,6 +629,8 @@ window.onload = async function() {
 
       // Show punk info
       await getPunkInfo(punkId);
+
+      if (blackList.includes(punkId)) showError("This NFT was blacklisted from this marketplace due to fraudlent activities. It will not be possible to buy or sell it.");
     }
 
     walletupdate();
