eb32a7d2745c4a7d3a9413f4863838867bca78a7
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2021-02-18 20:12:27 +0300

    deploy settings

--- smart_contracts/deploy/src/matcher.deploy.ts
+++ smart_contracts/deploy/src/matcher.deploy.ts
@@ -1,7 +1,7 @@
 import chai from "chai";
 import chaiAsPromised from 'chai-as-promised';
 import { ApiPromise, Keyring } from "@polkadot/api";
-import { IKeyringPair } from "@polkadot/types/types";
+import { IKeyringPair } from '@polkadot/types/types';
 import usingApi from "./substrate/substrate-api";
 import fs from "fs";
 import { Abi, BlueprintPromise as Blueprint, CodePromise, ContractPromise as Contract } from "@polkadot/api-contract";
@@ -14,8 +14,7 @@ const endowment = '100000000000000000';
 
 function deployBlueprint(alice: IKeyringPair, code: CodePromise): Promise<Blueprint> {
   return new Promise<Blueprint>(async (resolve, reject) => {
-    const unsub = await code
-      .createBlueprint()
+    const unsub = await code.createBlueprint()
       .signAndSend(alice, (result) => {
         if (result.status.isInBlock || result.status.isFinalized) {
           // here we have an additional field in the result, containing the blueprint
@@ -42,17 +41,21 @@ function instantiateContract(alice: IKeyringPair, blueprint: Blueprint) : Promis
 async function deployMatcherContract(api: ApiPromise): Promise<Contract> {
   const metadata = JSON.parse(fs.readFileSync('../market/target/metadata.json').toString('utf-8'));
   const abi = new Abi(metadata);
+  console.log('metadata ready');
 
   const keyring = new Keyring({ type: 'sr25519' });
   const deployer = keyring.addFromUri(`//Alice`);
 
   const wasm = fs.readFileSync('../market/target/matcher.wasm');
+  console.log('wasm ready');
 
   const code = new CodePromise(api, abi, wasm);
+  console.log('code ready');
 
   const blueprint = await deployBlueprint(deployer, code);
+  console.log('blueprint ready');
   const contract = (await instantiateContract(deployer, blueprint))['contract'] as Contract;
-
+  console.log('contract ready', contract);
   return contract;
 }
 
@@ -62,6 +65,7 @@ describe('Contracts', () => {
 
   it('Deploy matcher smart contract', async () => {
     await usingApi(async api => {
+      console.log('api ready');
 
       const contract = await deployMatcherContract(api);
       console.log("Contract address: ", contract.address.toString());
